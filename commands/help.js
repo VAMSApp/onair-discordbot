10da40c5580b7b5528d655c6e5ec3f66c140b609
Mike H. DeVita <mike@devita.co>
2022-10-21 19:44:29 -0700

    version 0.2.1

--- commands/help.js
+++ commands/help.js
@@ -1,39 +1,32 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, InteractionResponse, } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('help')
-        .setDescription('List all of my commands or info about a specific command.')
-        .addStringOption(option =>
-            option.setName('command')
-                .setDescription('The command you want info about.')
-                .setRequired(false)),
-    async execute(interaction) {
-        const commandName = interaction.options.getString('command');
+    .setName('help')
+    .setDescription('Replies with a list of available commands'),
+	async execute(interaction) {
+        let msg = ''
+        await interaction.deferReply({ ephemeral: true });
 
-        if (commandName) {
-            const command = interaction.client.commands.get(commandName)
-                || interaction.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-            if (!command) {
-                return interaction.reply({ content: 'That\'s not a valid command!', ephemeral: true });
+        const commands = interaction.client.commands.map(c => {
+            const data = c.data.toJSON()
+            return {
+                name: data.name,
+                description: data.description,
             }
+        })
 
-            const data = [];
-
-            if (command.name) data.push(`**Name:** ${command.name}`);
-            if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-            if (command.description) data.push(`**Description:** ${command.description}`);
-            if (command.usage) data.push(`**Usage:** ${interaction.client.prefix}${command.name} ${command.usage}`);
-
-            data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-
-            interaction.reply({ content: data.join('\n'), ephemeral: true });
-        } else {
-            const data = [];
-            data.push('Here\'s a list of all my commands:');
-            data.push(interaction.client.commands.map(command => command.name).join(', '));
-            interaction.reply({ content: data.join('\n'), ephemeral: true });
+        if (commands.length <= 0) {
+            msg = 'No commands available'
+            await interaction.editReply({ content: `\`\`\`\n${msg}\`\`\``, ephemeral: true });
         }
-    },
-};
+
+        msg += `**Available Commands**\n`
+
+        commands.forEach(({ name, description }) => {
+            msg += `/${name} - ${description}\n`
+        })
+
+        await interaction.editReply({ content: `\`\`\`\n${msg}\`\`\``, ephemeral: true });
+	}
+}
